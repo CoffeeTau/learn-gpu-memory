d011e3e460ea563fae85bf77f5cd2b043dc99708
qq_67606175 <qq_67606175@noreply.gitcode.com>
2026-09-08 20:36:38 +0800

    update

---
+++ 03_gradient.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+
+def show(name):
+    print(
+        f"{name:<20}",
+        f"{torch.cuda.memory_allocated()/1024**2:.1f} MB"
+    )
+
+model = nn.Linear(
+    10000,
+    10000,
+    bias=False
+).cuda() # FP32
+
+show("model created")
+
+x = torch.randn(
+    32,
+    10000,
+    device="cuda"
+)
+
+y = model(x)
+
+show("after forward")
+
+loss = y.sum()
+
+loss.backward()
+
+show("after backward")
+
+input()
