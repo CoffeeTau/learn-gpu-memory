9ec81e26df4e545bfc099a68e4999ad0df9b39b1
qq_67606175 <qq_67606175@noreply.gitcode.com>
2026-09-08 20:13:10 +0800

    update

---
+++ vim memory_01.py
@@ -0,0 +1,20 @@
+import torch
+import time
+
+device = "cuda"
+
+torch.cuda.empty_cache()
+
+print("Before:")
+print(torch.cuda.memory_allocated() / 1024**3, "GB")
+
+x = torch.empty(
+    1024, 1024, 1024,
+    dtype=torch.float32,
+    device=device
+) # 构造1024 * 1024 * 1024个FP32，也就是1B的FP32，
+
+print("After:")
+print(torch.cuda.memory_allocated() / 1024**3, "GB")
+
+time.sleep(30)
